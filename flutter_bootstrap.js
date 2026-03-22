ca4a3a72e332b31be877e63ca79ab7a222dd9bd8
SOETTRADER <lincoln.surveys25@gmail.com>
2026-03-22 23:17:59 +0300

    Add files via upload
    Release v2.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3828535476" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1556551500" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
